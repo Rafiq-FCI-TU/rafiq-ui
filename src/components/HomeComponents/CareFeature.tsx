8509ef116381ea94cb41c49f89ef8f5fc84717d6
Abdallh Elzorkany <abdoh5479@gmail.com>
2026-03-02 19:19:44 +0200

    made care card scale

--- src/components/HomeComponents/CareFeature.tsx
+++ src/components/HomeComponents/CareFeature.tsx
@@ -2,7 +2,7 @@ import type { Care } from "../../types/HomeCards";
 
 export default function CareFeature({ number, title, description }: Care) {
   return (
-    <section className="flex gap-4 border-l-5 border-primary-dark bg-linear-to-r from-gray-100 to-white p-5 rounded-lg">
+    <section className="flex gap-4 border-l-5 hover:scale-105 transition-all border-primary-dark bg-linear-to-r from-gray-100 to-white p-5 rounded-lg">
       <div className="bg-primary-dark size-10 p-4 font-semibold  rounded-full flex items-center justify-center text-white">
         {number}
       </div>
