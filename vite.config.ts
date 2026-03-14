1b209bc014c3cc95f0a544004d42fc82853daf4c
Abdallh Elzorkany <abdoh5479@gmail.com>
2026-03-14 21:42:28 +0200

    integration with netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react(), tailwindcss(), netlify()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
