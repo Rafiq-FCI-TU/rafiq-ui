84b9db024a3a50fb17d0249ba15af2e765879927
Abdallh Elzorkany <abdoh5479@gmail.com>
2026-05-02 18:00:07 +0300

    fix edit resource link validation

--- src/components/ResourceComponents/EditResourceForm.tsx
+++ src/components/ResourceComponents/EditResourceForm.tsx
@@ -34,7 +34,9 @@ export function EditResourceForm({
       !trimmedTitle ||
       !trimmedLink ||
       !trimmedDesc ||
-      !trimmedLink.match(/^https?:\/\//)
+      !trimmedLink.match(
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)$/,
+      )
     )
       return;
     const newTags = tags
@@ -91,7 +93,9 @@ export function EditResourceForm({
             !title.trim() ||
             !link.trim() ||
             !description.trim() ||
-            !link.match(/^https?:\/\//)
+            !link.match(
+              /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)$/,
+            )
           }
           className="cursor-pointer px-4 py-2 text-sm font-medium bg-primary text-white rounded-lg hover:bg-primary-dark transition-all disabled:opacity-50 disabled:cursor-not-allowed flex items-center gap-1.5 shadow-sm"
         >
